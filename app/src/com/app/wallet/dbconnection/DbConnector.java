21698c6294b71eb6f1eb7a00aa990d7445e462c3
vaibhav1510 <vaibhav@chargebee.com>
2017-11-16 23:22:05 +0530

    change

---
+++ app/src/com/app/wallet/dbconnection/DbConnector.java
@@ -0,0 +1,5 @@
+package com.app.wallet.dbconnection;
+
+public class DbConnector {
+
+}
---
+++ app/src/com/app/wallet/models/User.java
@@ -0,0 +1,8 @@
+package com.app.wallet.models;
+
+import com.app.wallet.dbconnection.DbConnector;
+
+public class User {
+public static void main (String[] args) {
+}
+}
